5b1660431a2e620101949b34d0aa3bfe4e8cd197
gtd97 <genistorres97@gmail.com>
2018-07-24 17:38:36 +0200

    push final

---
+++ InyeccionConversionJavaCDI/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace InyeccionConversionJavaCDI
+{
+    class Program
+    {
+        private static IContainer Container{ get; set; }
+
+        static void Main(string[] args)
+        {
+            ConfigureContainer();
+            Resolucion();
+            
+            Console.ReadLine();
+        }
+
+
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<BMWAutoService>().SingleInstance();
+            builder.RegisterType<FordAutoService>().SingleInstance();
+            builder.RegisterType<HondaAutoService>().SingleInstance();
+
+            Container = builder.Build();
+        }
+
+
+        public static void Resolucion()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var bmw = scope.Resolve<BMWAutoService>();
+                var ford = scope.Resolve<FordAutoService>();
+                var honda = scope.Resolve<HondaAutoService>();
+
+                AutoServiceCallerImp serviceCaller = new AutoServiceCallerImp(bmw, honda, ford);
+                serviceCaller.callAutoService();
+            }
+        }
+        
+    }
+}
---
+++ InyeccionConversionJavaCDIviaXML/AutoServiceCallerImp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionConversionJavaCDIviaXML
+{
+    public class AutoServiceCallerImp: AutoServiceCaller
+    {
+        private AutoService BMWAutoService;
+        private AutoService FordAutoService;
+        private AutoService HondaAutoService;
+
+        public AutoServiceCallerImp(AutoService BMWAutoService, AutoService FordAutoService, AutoService HondaAutoService)
+        {
+            this.BMWAutoService = BMWAutoService;
+            this.FordAutoService = FordAutoService;
+            this.HondaAutoService = HondaAutoService;
+        }
+
+        public void callAutoService()
+        {
+            BMWAutoService.getService();
+
+            FordAutoService.getService();
+
+            HondaAutoService.getService();
+        }
+    }
+}
